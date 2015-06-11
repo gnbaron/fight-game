d3f8e7a6f39f4d2fd21d546f683f25f005b89844
Lucas Krüger <lucas.kr1996@gmail.com>
2015-06-11 09:00:09 -0300

    added monster spawn event

--- gameserver.js
+++ gameserver.js
@@ -1,15 +1,17 @@
+var heros = ['../images/batman.png', '../images/superman.png', '../images/ironman.png', '../images/greenlantern.png', '../images/spiderman.png'];
+
 module.exports = function (socket) {
-
-  console.log('player connected');
-
   socket.broadcast.emit('player:enter', socket.handshake.address);
 
   socket.on('player:move', function (data) {
     socket.broadcast.emit('player:move', data);
   });
 
+  socket.on('monster:spawn', function (data) {
+    socket.broadcast.emit('monster:spawn', data);
+  });
+
   socket.on('disconnect', function () {
-    console.log('player disconnected')
     socket.broadcast.emit('player:left', socket.handshake.address);
   });
 
